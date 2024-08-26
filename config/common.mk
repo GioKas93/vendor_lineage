23d2afd41e0c7b387f12b9540b792c99752c6eb5
GioKas93 <giokas93@pm.me>
2024-08-27 00:02:50 +0200

    Inline Gapps
    Change-Id: Ibfbda2eff0e586de01b810b684aa64bac8072af5

--- config/common.mk
+++ config/common.mk
@@ -252,3 +252,8 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# GAPPS
+ifeq ($(WITH_GMS),true)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+endif
